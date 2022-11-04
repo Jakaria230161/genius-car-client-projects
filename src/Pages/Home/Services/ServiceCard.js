035ee858f7456f830654ad2be5e79be6705d3a2f
Jakaria230161 <jakariamahmud016@gmail.com>
2022-11-04 10:31:37 +0600

    firebase setup

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const ServiceCard = ({ service }) => {
   const {img, price, title} = service;
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
       <figure>
-        <img src={img}alt="Shoes" />
+        <img src={img} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <p className="text-2xl text-orange-600 font-bold">Price: ${price}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="text-orange-600">
+            <FaArrowRight />{" "}
+          </button>
         </div>
       </div>
     </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ServiceCard from './ServiceCard';
 
+
 const Services = () => {
   const [services, setServices] = useState([]);
   useEffect(() => {
@@ -11,20 +12,22 @@ const Services = () => {
   
   return (
     <div>
-      <div className='text-center my-4'>
-        <p className='text-orange-600 text-2xl font-bold'>Services</p>
-        <h2 className='text-5xl font-semibold'>Our Service Area</h2>
-        <p className='py-4'>The majority have suffered alteration in some form, by injected humour, or randomized <br />
-          words which don't look even slightly believable. </p>
+      <div className="text-center my-4">
+        <p className="text-orange-600 text-2xl font-bold">Services</p>
+        <h2 className="text-5xl font-semibold">Our Service Area</h2>
+        <p className="py-4">
+          The majority have suffered alteration in some form, by injected
+          humour, or randomized <br />
+          words which don't look even slightly believable.{" "}
+        </p>
       </div>
-      <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-        {
-          services.map(service => <ServiceCard
-            key={service._id}
-            service={service}
-
-          ></ServiceCard>)
-        }
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))}
+      </div>
+      <div className='text-center my-8'>
+        <button className="btn btn-outline btn-warning">More Services</button>
       </div>
     </div>
   );
